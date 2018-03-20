1be2b726b2e3899818ed323894a8f27e58c4df13
Subin Varghese <subinvarghese99@rediff.com>
2018-03-20 14:57:51 +0530

    JSON View

---
+++ Back End/SWAPI/star_wars.js
@@ -0,0 +1,22 @@
+var show = document.getElementById("show_btn");
+var div = document.getElementById("json_div");
+const urls = [
+  'https://swapi.co/api/people/',
+  'https://swapi.co/api/planets/',
+  'https://swapi.co/api/starships/'
+]
+
+function showResult() {
+	console.log("BTN");
+	Promise.all(urls.map(url => {
+	return fetch(url).then(resp => resp.json())}))
+.then(data => {
+	document.getElementById("json_div").innerHTML = JSON.stringify(data[0], undefined, 2);
+	document.getElementById("json_div").innerHTML += JSON.stringify(data[1], undefined, 2);
+	document.getElementById("json_div").innerHTML += JSON.stringify(data[2], undefined, 2);
+})
+.catch(err => console.log('error',err));
+}
+
+show.addEventListener("click", showResult);
+
